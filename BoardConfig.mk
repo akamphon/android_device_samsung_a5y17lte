769d102ced2fb1f69bbea3e28cf86f5f1bf026aa
Christopher N. Hesse <raymanfx@gmail.com>
2018-07-19 23:12:09 +0200

    a5y17lte: Add device specific DTBH header
    Change-Id: I5fbb37904478121e1d949c8431253914f8dd25b8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,8 +18,8 @@ include device/samsung/universal7880-common/BoardConfigCommon.mk
 
 LOCAL_PATH := device/samsung/a5y17lte
 
-# Extracted with libbootimg
-BOARD_KERNEL_PREBUILT_DT := $(LOCAL_PATH)/dtb.img
+# Include path
+TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
 
 # Kernel config
 TARGET_KERNEL_CONFIG := lineage-a5y17lte_defconfig
